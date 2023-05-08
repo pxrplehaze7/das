268e973255e80e2ca31431980bfd209647a02833
Belen <belen.gonzalez.d@outlook.com>
2023-05-07 20:36:32 -0400

    PDF se guarda segun corresponde

--- controller/procesoDatos.php
+++ controller/procesoDatos.php
@@ -22,14 +22,18 @@ $ruta = 'pdfs/';
 // Crear una carpeta con el valor del input de tipo texto
 if (!file_exists($ruta . $rutPersona)) {
     mkdir($ruta . $rutPersona, 0777, true);
+
+    // Crear subcarpetas para los archivos AFP y de nacimiento
+    mkdir($ruta . $rutPersona . '/AFP/', 0777, true);
+    mkdir($ruta . $rutPersona . '/NACIMIENTO/', 0777, true);
+
 }
   
   // Mover los archivos PDF a la nueva ubicación
-  move_uploaded_file($_FILES['nameAFPdoc']['tmp_name'], $ruta . $rutPersona . '/' . $pdfAFP);
-  move_uploaded_file($_FILES['nameNACdoc']['tmp_name'], $ruta . $rutPersona . '/' . $pdfNacimiento);
+  move_uploaded_file($_FILES['nameAFPdoc']['tmp_name'], $ruta . $rutPersona . '/AFP/' . $pdfAFP);
+  move_uploaded_file($_FILES['nameNACdoc']['tmp_name'], $ruta . $rutPersona . '/NACIMIENTO/' . $pdfNacimiento);
   
   echo "Archivos guardados correctamente en la ruta: " . $ruta . $rutPersona;
-
 
 
 
